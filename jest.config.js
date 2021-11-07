73a2f321f8e70dec7c1ba668a20ff28344a31b07
Javier García González <javier@garciagon.com>
2021-11-07 20:01:17 +0100

    Use JS file for jest config instead of TS

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,10 @@
-export default {
+const baseConfig = require('./jest.base.config');
+
+module.exports = {
+    ...baseConfig,
     coverageDirectory: 'coverage',
     testMatch: ['<rootDir>/services/*/__tests__/*.test.ts', '<rootDir>/packages/*/__tests__/*.test.ts'],
-    preset: 'ts-jest/presets/js-with-babel',
-    testPathIgnorePatterns: ['/node_modules/', 'lib'],
     roots: ['<rootDir>/packages', '<rootDir>/services'],
-    globals: {
-        'ts-jest': {
-            babelConfig: true
-        }
-    },
-    testEnvironment: 'jest-environment-node',
-    transform: {
-        '^.+\\.js$': 'babel-jest',
-        '^.+\\.ts$': 'ts-jest'
-    },
     projects: [
         { displayName: 'Service tests', testMatch: ['<rootDir>/services/*/__tests__/*.test.ts'] },
         { displayName: 'Package tests', testMatch: ['<rootDir>/packages/*/__tests__/*.test.ts'] }
