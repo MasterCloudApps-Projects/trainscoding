49ef58f053b45c8b8d586baa4b056203f13dd5a1
Javier García González <javier@garciagon.com>
2021-11-16 21:12:14 +0100

    Keep jest within the global scope and not inside services

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
-const baseConfig = require('./jest.base.config');
-
 module.exports = {
-    ...baseConfig,
+    preset: 'ts-jest/presets/js-with-babel',
+    globals: {
+        'ts-jest': {
+            babelConfig: true
+        }
+    },
+    testEnvironment: 'jest-environment-node',
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.ts$': 'ts-jest'
+    },
     coverageDirectory: 'coverage',
     testMatch: ['<rootDir>/services/*/__tests__/*.test.ts', '<rootDir>/packages/*/__tests__/*.test.ts'],
     roots: ['<rootDir>/packages', '<rootDir>/services'],
